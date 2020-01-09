12bd69f8c3333eb381cae5221b900dd12827529c
BironSu <bironsu@pursuit.org>
2020-01-08 23:01:30 -0500

    Adding necessary items for main login view

--- FirebaseLogin/LoginViews/LoginView.swift
+++ FirebaseLogin/LoginViews/LoginView.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class LoginView: UIView {
 
+    lazy var logoImage: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "LogoPlaceholder"))
+        
+        return image
+    }()
+    lazy var loginTextView: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Username"
+        textField.layer.cornerRadius = 10.0
+        return textField
+    }()
+    lazy var passwordTextView: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.layer.cornerRadius = 10.0
+        return textField
+    }()
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.layer.cornerRadius = 10.0
+        return button
+    }()
+    lazy var signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("SignUp", for: .normal)
+        button.layer.cornerRadius = 10.0
+        return button
+    }()
+    lazy var forgotPassword: UIButton = {
+        let button = UIButton()
+        button.setTitle("Forgot Password?", for: .normal)
+        button.layer.cornerRadius = 10.0
+        button.backgroundColor?.withAlphaComponent(0.2)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: UIScreen.main.bounds)
         setupView()
-        self.backgroundColor = .blue
+        self.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
     }
     
     required init?(coder: NSCoder) {
@@ -24,21 +61,47 @@ class LoginView: UIView {
 
 extension LoginView {
     private func setupView() {
-        
+        setupLogoImageView()
+        setupLoginTextField()
+        setupLoginPasswordTextField()
+        setupLoginButton()
+        setupSignUpButton()
+        setupForgotPasswordButton()
+    }
+    func setupLogoImageView(){
+        addSubview(logoImage)
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        ])
     }
     func setupLoginTextField(){
-        
+        addSubview(loginTextView)
+        loginTextView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        ])
     }
     func setupLoginPasswordTextField(){
-        
+        addSubview(passwordTextView)
+        passwordTextView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        ])
     }
     func setupLoginButton(){
-        
+        addSubview(loginButton)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        ])
     }
     func setupSignUpButton(){
-        
+        addSubview(signUpButton)
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        ])
     }
     func setupForgotPasswordButton(){
-        
+        addSubview(forgotPassword)
+        forgotPassword.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        ])
     }
 }
