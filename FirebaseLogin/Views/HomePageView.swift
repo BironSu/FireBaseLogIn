f04c59929eff0a23f6a52c7590b74c58382ae563
BironSu <bironsu@pursuit.org>
2020-01-10 15:50:44 -0500

    Setting homePage placeholder

---
+++ FirebaseLogin/Views/HomePageView.swift
@@ -0,0 +1,21 @@
+//
+//  HomePageView.swift
+//  FirebaseLogin
+//
+//  Created by Biron Su on 1/10/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class HomePageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
