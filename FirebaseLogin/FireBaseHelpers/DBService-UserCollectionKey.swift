89ebc3fd3ef91f0a29a711324ecc48af544450c5
BironSu <bironsu@pursuit.org>
2020-02-21 15:33:47 -0500

    Added create user and fetch user function to DBService extension

--- FirebaseLogin/FireBaseHelpers/DBService-UserCollectionKey.swift
+++ FirebaseLogin/FireBaseHelpers/DBService-UserCollectionKey.swift
@@ -19,3 +19,37 @@ struct UserCollectionKeys {
     static let CoverImageKey = "photoURL"
     
 }
+
+extension DBService {
+    static public func createUser(user: UserModel, completion: @escaping (Error?)-> Void) {
+        firestoreDB.collection(UserCollectionKeys.CollectionKey)
+            .document(user.userID)
+            .setData([
+                UserCollectionKeys.UserIDKey: user.userID,
+                UserCollectionKeys.EmailKey: user.email,
+                UserCollectionKeys.JoinedDateKey: user.joinedDate,
+                UserCollectionKeys.DisplayNameKey: user.displayName,
+                UserCollectionKeys.CoverImageKey: user.photoURL ?? ""
+            ]) { (error) in
+                if let error = error {
+                    completion(error)
+                } else {
+                    completion(nil)
+                }
+        }
+    }
+    static public func fetchUser(userID: String, completion: @escaping(Error?, UserModel?) -> Void) {
+        DBService.firestoreDB
+            .collection(UserCollectionKeys.CollectionKey)
+            .whereField(UserCollectionKeys.UserIDKey, isEqualTo: userID)
+            .getDocuments { (snapshot, error) in
+                if let error = error {
+                    completion(error, nil)
+                } else if let snapshot = snapshot?.documents.first {
+                    let fetchedUser = UserModel(dict: snapshot.data())
+                    completion(nil, fetchedUser)
+                }
+        }
+        
+    }
+}
